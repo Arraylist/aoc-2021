b1291ac0eb5e081c9195e49d958a49ea34169371
Arraylist <nguyenlam231992@gmail.com>
2021-12-06 20:39:06 +0100

    chore(1): refactor to prepare for part 2

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,18 +1,21 @@
 fn main() {
-    let mut count = 0;
-    let mut prev = -1;
     let depths: Vec<i32> = include_str!("./input.txt")
         .trim()
         .split("\n")
         .map(|s| s.parse::<i32>().unwrap())
         .collect();
 
+    println!("{}", part_1(&depths));
+}
+
+fn part_1(depths: &Vec<i32>) -> i32 {
+    let mut count = 0;
+    let mut prev = -1;
     for depth in depths {
-        if depth > prev && prev != -1 {
+        if depth > &prev && prev != -1 {
             count += 1;
         }
-        prev = depth;
+        prev = *depth;
     }
-
-    println!("{}", count);
+    return count;
 }
