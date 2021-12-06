ecd60ac7f1e1a0009ef40faa584c15d015983843
Arraylist <nguyenlam231992@gmail.com>
2021-12-06 21:25:51 +0100

    feat(2): part 1

---
+++ 2/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let instructions: Vec<Vec<&str>> = include_str!("./input.txt")
+        .trim()
+        .split("\n")
+        .into_iter()
+        .map(|s| s.split(' ').collect())
+        .collect();
+
+    println!("{}", part_1(&instructions));
+}
+
+fn to_int(s: &str) -> i32 {
+    return s.parse::<i32>().unwrap();
+}
+
+fn part_1(instructions: &Vec<Vec<&str>> ) -> i32 {
+    let mut horizontal = 0;
+    let mut depth = 0;
+    for instruction in instructions {
+        if instruction[0] == "forward" {
+            horizontal += to_int(instruction[1]);
+        } else if instruction[0] == "down" {
+            depth += to_int(instruction[1]);
+        } else if instruction[0] == "up" {
+            depth -= to_int(instruction[1]);
+        }
+    }
+    return horizontal * depth;
+}
