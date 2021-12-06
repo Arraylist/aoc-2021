fa64fbb8fec9b56b27eaf8d8391a60d77c8eaa20
Arraylist <nguyenmlam231992@gmail.com>
2021-12-06 20:30:00 +0100

    feat(1): use for in instead

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,16 +1,18 @@
 fn main() {
     let mut count = 0;
     let mut prev = -1;
-    let input = include_str!("./input.txt")
+    let depths: Vec<i32> = include_str!("./input.txt")
         .trim()
         .split("\n")
-        .for_each(|depth| {
-            let intDepth = depth.parse().unwrap();
-            if intDepth > prev && prev != -1 {
-                count += 1;
-            }
-            prev = intDepth;
-        });
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+
+    for depth in depths {
+        if depth > prev && prev != -1 {
+            count += 1;
+        }
+        prev = depth;
+    }
 
     println!("{}", count);
 }
