e8c9831f907f8364dce35b8fbb80a9018a2f123e
Arraylist <nguyenlam231992@gmail.com>
2021-12-06 20:57:44 +0100

    feat(1): day 1 part 2

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
         .collect();
 
     println!("{}", part_1(&depths));
+    println!("{}", part_2(&depths));
 }
 
 fn part_1(depths: &Vec<i32>) -> i32 {
@@ -16,6 +17,22 @@ fn part_1(depths: &Vec<i32>) -> i32 {
             count += 1;
         }
         prev = *depth;
+    }
+    return count;
+}
+
+fn part_2(depths: &Vec<i32>) -> i32 {
+    let mut count = 0;
+    let mut prev = -1;
+    for (i, depth) in depths.iter().enumerate() {
+        if (i + 2) >= depths.len() {
+            break;
+        }
+        let sum = depth + depths[i + 1] + depths[i + 2];
+        if sum > prev && prev != -1 {
+            count += 1;
+        }
+        prev = sum;
     }
     return count;
 }
